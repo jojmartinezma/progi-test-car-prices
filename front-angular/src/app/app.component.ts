c6288c6d7568ac3765d1eaa7632198b0c465e029
jojmartinezma <julianm_10@hotmail.com>
2024-02-18 10:28:29 -0500

    create form

--- front-angular/src/app/app.component.ts
+++ front-angular/src/app/app.component.ts
@@ -1,14 +1,48 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { RouterOutlet } from '@angular/router';
+import { FormControl, FormGroup, FormsModule, ReactiveFormsModule, Validators } from '@angular/forms';
+import { MatCardModule } from '@angular/material/card';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { MatSelectModule } from '@angular/material/select';
+import { MatButtonModule } from '@angular/material/button';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet],
+  imports: [
+    CommonModule,
+    ReactiveFormsModule,
+    FormsModule,
+    MatCardModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatSelectModule,
+    MatButtonModule
+  ],
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  title = 'front-angular';
+export class AppComponent implements OnInit{
+
+  public vehicleFeesForm!: FormGroup;
+
+  constructor() {
+
+  }
+
+  ngOnInit(): void {
+    this.initFormGroup();
+  }
+
+  private initFormGroup() {
+    this.vehicleFeesForm = new FormGroup({
+      vehicleBasePrice: new FormControl('', [ Validators.required, Validators.pattern(/^\d+$/)]),
+      vehicleType: new FormControl(null,[ Validators.required ])
+    })
+  }
+
+  public processForm() {
+
+  }
 }
